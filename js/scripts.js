25f41b47435dea3ac4497486711c5f401520c226
Travis Toal <travisty12@gmail.com>
2019-01-18 11:18:00 -0800

    flesh out branches

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 var name;
+var preferCss = false;
+var atBack = false;
 $(document).ready(function() {
   $("#page-body").children("div").toggle();
   $("#name-in").click(function() {
@@ -8,21 +10,57 @@ $(document).ready(function() {
   $("#name-form").submit(function(event) {
     name = $("#name-in").val();
     $("#nf-holder").fadeOut();
-    $("#second").fadeIn();
+    $("#start").fadeIn();
     $("#front").click(function() {
-      $("#second").fadeOut();
+      $("#start").fadeOut();
       $("#f-choice").fadeIn();
       $("#layout").click(function() {
+        $("#f-choice").fadeOut();
+        $("#css-text").fadeIn();
 
       });
       $("#engine").click(function() {
-        
+        $("#f-choice").fadeOut();
+        $("#b-choice").fadeIn();
+        preferCss = true;
+        atBack = true;
       });
     });
     $("#back").click(function() {
-      $("#second").fadeOut();
+      $("#start").fadeOut();
       $("#b-choice").fadeIn();
     });
+    if (atBack) {
+      $("#app").click(function() {
+        $("#b-choice").fadeOut();
+        $("#app-choice").fadeIn();
+        $("#business").click(function() {
+          $("#app-choice").fadeOut();
+          $("#java-text").fadeIn();
+        });
+        $("#startup").click(function() {
+          $("#app-choice").fadeOut();
+          $("#ruby-text").fadeIn();
+        });
+      });
+      $("#server").click(function() {
+        $("#b-choice").fadeOut();
+        $("#server-choice").fadeIn();
+        $("#consistency").click(function() {
+          $("#server-choice").fadeOut();
+          $("#c-sharp-text").fadeIn();
+        });
+        $("#versatility").click(function() {
+          $("#server-choice").fadeOut();
+          $("#php-text").fadeIn();
+        });
+      });
+      if (preferCss) {
+        $("css-optional").show();
+        $("#css-text").show();
+      };
+    };
+
     event.preventDefault();
   });
 
