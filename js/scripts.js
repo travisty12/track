cceecf398c2cb2c9e8e8fefcdb7f227ebab459a6
Travis Toal <travisty12@gmail.com>
2019-01-18 10:01:55 -0800

    work on branching logic in quiz

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,28 @@
 var name;
 $(document).ready(function() {
+  $("#page-body").children("div").toggle();
+  $("#name-in").click(function() {
+    $("#questions").fadeOut();
+    $("#intro").fadeOut();
+  });
   $("#name-form").submit(function(event) {
     name = $("#name-in").val();
-    $("#name-form").hide();
-    $("#test-form").show();
+    $("#nf-holder").fadeOut();
+    $("#second").fadeIn();
+    $("#front").click(function() {
+      $("#second").fadeOut();
+      $("#f-choice").fadeIn();
+      $("#layout").click(function() {
+
+      });
+      $("#engine").click(function() {
+        
+      });
+    });
+    $("#back").click(function() {
+      $("#second").fadeOut();
+      $("#b-choice").fadeIn();
+    });
     event.preventDefault();
   });
 
